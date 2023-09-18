dc5baddc149092cc6d4e2a4f9228ffc61dea38e6
Manuel Nunes <122288176+Manuel-Nunes@users.noreply.github.com>
2023-09-18 22:53:52 +0200

    Add Files from other repo;

---
+++ src/Components/HomeComponents/NavBar.tsx
@@ -0,0 +1,12 @@
+// import styled from 'styled-components';
+
+// const 
+export function NavBar(){
+  return (
+    <>
+      <p>LOL NavBar</p>
+    </>
+  );
+}
+
+export default NavBar;
---
+++ src/Screens/CatShowCase.tsx
@@ -0,0 +1,9 @@
+export function CatShowCase(){
+  return (
+    <>
+      <p>Hello Cat Show case</p>
+    </>
+  );
+}
+
+export default CatShowCase;
