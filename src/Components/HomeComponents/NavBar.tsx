4c8a97a7671dfa9c5fb7c96cd8725b3e9ba0a375
Manlx <marnununes@gmail.com>
2023-09-19 01:56:43 +0200

    Add Menu Items;

--- src/Components/HomeComponents/NavBar.tsx
+++ src/Components/HomeComponents/NavBar.tsx
@@ -1,11 +1,49 @@
-// import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
-// const 
+const NavItem = styled.div`
+  font-size:1.5em;
+  display: flex;
+  box-sizing:border-box;
+  background-color: var(--NavItemColor);
+  padding: 0.75em 1em;
+  width: 100%;
+  border-radius: 20px 0 20px 0;
+  justify-content: center;
+  align-items: center;
+
+  &:hover{
+    filter:brightness(1.2);
+  }
+`;
+
+const NavContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 30%;
+  height: 75%;
+  gap: 3rem;
+  /* justify-content:space-evenly; */
+  /* background-color: green; */
+`;
+
+
 export function NavBar(){
+  const navigate = useNavigate();
   return (
-    <>
-      <p>LOL NavBar</p>
-    </>
+    <NavContainer>
+      <NavItem onClick={()=>{
+        navigate('/Academics');
+      }}>
+        Academics
+      </NavItem>
+
+      <NavItem onClick={()=>{
+        navigate('/CatShowCase');
+      }}>
+        Feline Flex
+      </NavItem>
+    </NavContainer>
   );
 }
 
--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -1,43 +1,73 @@
-import { ContainerFCC, CustomImage, NavBar } from 'Components';
+import { 
+  CustomImage,
+  NavBar
+} from 'Components';
+
 import styled from 'styled-components';
 
 import ProfilePic from '../assets/ProfilePic.png';
+import Employer from '../assets/Employer.png';
 
 const BigDiv = styled.div`
   display: flex;
   flex-direction: column;
   background-color: var(--profileCardColor);
   /* height: 45rem; */
-  height: 80vh;
+  height: var(--ProfileHeight);
   width: 25rem ;
-  border-radius: 0 0 15rem 0;
+  border-radius: var(--ProfileBorderRadius);
   overflow: hidden;
   align-items: center;
   box-sizing:border-box;
   z-index: 1;
+  gap: 1rem;
+  padding-top: 1rem;
 `;
 
 const InfoWrapper = styled.div`
   background-color: var(--infoBlockColor);
-  /* height: 45rem; */
-  height: 80vh;
-  /* height: 48rem; */
-  width: 28rem;
+  height: var(--ProfileHeight);
+  width: 43rem;
+  translate: -15rem;
   left: 0;
   position: absolute;
   z-index: 0;
-  border-radius: 0 0 15rem 0;
-  border-radius: 0 0 15rem 0;
-  /* border-radius: 0 0 300px 0; */
+  border-radius: var(--ProfileBorderRadius);
 
   box-sizing: border-box;
-  padding: 0 5rem 0 0;
+  padding: 0 3rem 0 0;
   display: flex;
   flex-direction: column;
   align-items: end;
+
+  justify-content: center;
+
   &:hover{
-    width: 43rem;
+    translate: 0;
   }
+`;
+
+const CIEmployerWrapper = styled.div`
+  display: none;
+
+  @media screen and (height > 750px )  {
+    display: flex;
+  }
+`;
+
+const H3EmployerWrapper = styled.div`
+  display: flex;
+  font-size: 2rem;
+  font-weight: 600;
+
+  @media screen and (height > 750px )  {
+    display: none;
+  }
+`;
+
+const BBDColor = styled.div`
+  color: red;
+  display: inline-block;
 `;
 
 const Wrapper = styled.div`
@@ -48,19 +78,30 @@ export function Home(){
   return (
     <Wrapper>
       <BigDiv>
-        <ContainerFCC style={
-          {
-            width:'25rem',
-            aspectRatio:'1/1'
-          }
-        }>
+
           <CustomImage $src={ProfilePic} $size='20rem' $bordeRaduis='5rem'/>
-        </ContainerFCC>
+
         <h1>Manuel A Nunes</h1>
+        <h2>Employed At:</h2>
+
+        <CIEmployerWrapper>
+
+          <CustomImage $src={Employer} $size='15rem' $bordeRaduis='5rem'/>
+
+        </CIEmployerWrapper>
+
+        <H3EmployerWrapper>
+          <BBDColor>{'BBD '}</BBDColor>
+          Software
+        </H3EmployerWrapper>
       </BigDiv>
+
       <InfoWrapper>
+
         <NavBar />
+
       </InfoWrapper>
+
     </Wrapper>
   );
 }
